e795074baaccee5676d61dcb321f72d3cfdc0f59
Kaustav Chakraborty <kaustavchakraborty99@gmail.com>
2026-03-02 12:10:54 +0530

    Fixed WebDriverFactory - CI ready

--- src/main/java/utils/webDriverFactory.java
+++ src/main/java/utils/webDriverFactory.java
@@ -1,18 +1,22 @@
 package utils;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
 public class webDriverFactory {
-    public webDriverFactory(){}
-    public static  String remote=System.getProperty("remote");
+
+    public webDriverFactory() {}
+
+    public static String remote = System.getProperty("remote");
 
     // Entry point
-    public static WebDriver createWebDriver( browser browser) {
+    public static WebDriver createWebDriver(browser browser) {
 
         if ("true".equalsIgnoreCase(remote)) {
             return getRemoteDriver(browser);
@@ -23,10 +27,10 @@ public class webDriverFactory {
 
     // Local driver creation
     private static WebDriver getLocalDriver(browser browser) {
+
         WebDriver driver;
 
         switch (browser) {
-
 
             case EDGE:
                 driver = new EdgeDriver();
@@ -36,16 +40,31 @@ public class webDriverFactory {
                 driver = new FirefoxDriver();
                 break;
 
+            case CHROME:
             default:
-                driver=new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+
+                // Headless settings for CI (GitHub Actions / Linux)
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+                options.addArguments("--disable-notifications");
+
+                driver = new ChromeDriver(options);
+                break;
         }
-        driver.manage().window().maximize();
+
+        // Common settings
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        // Set window size explicitly (works in headless)
+        driver.manage().window().setSize(new Dimension(1920, 1080));
 
         return driver;
     }
 
-    // Remote driver (stub for now)
+    // Remote driver (for Selenium Grid / cloud - future use)
     private static WebDriver getRemoteDriver(browser browser) {
         throw new UnsupportedOperationException("Remote execution not implemented yet");
     }
